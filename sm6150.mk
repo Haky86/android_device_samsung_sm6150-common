bf4eb0138082f2993211e5edeab87785efaa8f04
Jesse Chan <jc@lineageos.org>
2020-10-08 15:08:40 +0700

    sm6150-common: Import pixel libperfmgr Power HAL
    * from hardware/samsung
    
    Signed-off-by: Jesse Chan <jc@lineageos.org>
    Change-Id: I2f2c1f9f9ce7a48d6c5108c022947fb9e263d3b6

--- sm6150.mk
+++ sm6150.mk
@@ -43,6 +43,13 @@ PRODUCT_PACKAGES += \
 # OTA Updater
 AB_OTA_UPDATER := false
 
+# Power
+PRODUCT_PACKAGES += \
+    android.hardware.power@1.3-service.sm6150
+
+PRODUCT_COPY_FILES += \
+    $(COMMON_PATH)/configs/powerhint.json:$(TARGET_COPY_OUT_PRODUCT)/vendor_overlay/$(PLATFORM_VNDK_VERSION)/etc/powerhint.json
+
 # Recovery
 PRODUCT_COPY_FILES += \
     $(COMMON_PATH)/recovery/root/init.recovery.qcom.rc:root/init.recovery.qcom.rc
